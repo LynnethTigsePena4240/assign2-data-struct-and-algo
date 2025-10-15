de8aafdb31e271de6e548f6347f50cd7286c2d31
Annie Nguyen <a.nguyen2@hotmail.com>
2025-10-15 19:55:04 -0400

    added javaFX

--- src/assign2/Assign2.java
+++ src/assign2/Assign2.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package assign2;
 
 import javafx.application.Application;
@@ -12,7 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -29,10 +27,26 @@ public class Assign2 extends Application{
         Label lname = new Label("Name:");
         TextField name = new TextField();        
         
+        Label lphone = new Label("Phone Number:");
+        TextField phone = new TextField();
+        
         Label lcake_type = new Label("Choose Cake Type");
-	ComboBox<String> comboBox = new ComboBox<>();
-	comboBox.getItems().addAll("Apple", "Carrot", "Cheesecake", "Chocolate","Coffe","Opera","Tiramisu"); //this will return all of the choices in the comboBox
-	comboBox.setPromptText("Select one");
+        ComboBox<String> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll("Apple", "Carrot", "Cheesecake", "Chocolate","Coffee","Opera","Tiramisu");
+        comboBox.setPromptText("Select one");
+        
+        Label lcake_size = new Label("Choose Cake Size");
+        
+        ToggleGroup sizeGroup = new ToggleGroup();
+        RadioButton smallRadio = new RadioButton("Small");
+        RadioButton mediumRadio = new RadioButton("Medium");
+        RadioButton largeRadio = new RadioButton("Large");
+        
+        smallRadio.setToggleGroup(sizeGroup);
+        mediumRadio.setToggleGroup(sizeGroup);
+        largeRadio.setToggleGroup(sizeGroup);
+        
+        HBox sizeBox = new HBox(10, smallRadio, mediumRadio, largeRadio);
         
         Button quitBtn = new Button("Quit");
         quitBtn.setStyle("-fx-background-color: #add8e6;");
@@ -47,18 +61,18 @@ public class Assign2 extends Application{
         Button saveBtn = new Button("Save");
         saveBtn.setStyle("-fx-background-color: #add8e6;");
         
-        //Hboxs
-        HBox hname = new HBox(15,lname,name);
+        HBox hname = new HBox(15, lname, name);
+        HBox hphone = new HBox(15, lphone, phone);
         
-        HBox btnBox = new HBox(10,quitBtn, saveBtn );
+        HBox btnBox = new HBox(10, quitBtn, saveBtn);
              
-        VBox vbox = new VBox(15,hname,lcake_type ,comboBox, btnBox);
+        VBox vbox = new VBox(15, hname, hphone, lcake_type, comboBox, lcake_size, sizeBox, btnBox);
         vbox.setPadding(new Insets(15, 12, 13, 14));
         
-        Scene scene = new Scene(vbox,400,200);
+        Scene scene = new Scene(vbox, 400, 300);
         
         stage.setScene(scene);
-        stage.setTitle("Bakery: Customers’ Orders");
+        stage.setTitle("Bakery: Customers' Orders");
         stage.show();
     }
 
@@ -66,8 +80,6 @@ public class Assign2 extends Application{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         launch(args);
     }
-    
 }
