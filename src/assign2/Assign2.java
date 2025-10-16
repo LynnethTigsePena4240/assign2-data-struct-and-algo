3428d97c6a6d822aefd10a611cfd55a99f66297f
AshtonPhillips2005 <ashtongamer11@gmail.com>
2025-10-16 13:01:57 -0400

    Added Delivery Area Checkbox Feature

--- src/assign2/Assign2.java
+++ src/assign2/Assign2.java
@@ -6,11 +6,13 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox; 
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert; 
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,63 +21,74 @@ import javafx.stage.Stage;
  *
  * @author lynne
  */
-public class Assign2 extends Application{
-    
+public class Assign2 extends Application {
+
     @Override
-    public void start(Stage stage){
-        
+    public void start(Stage stage) {
+
         Label lname = new Label("Name:");
-        TextField name = new TextField();        
-        
+        TextField name = new TextField();
+
         Label lphone = new Label("Phone Number:");
         TextField phone = new TextField();
-        
+
         Label lcake_type = new Label("Choose Cake Type");
         ComboBox<String> comboBox = new ComboBox<>();
-        comboBox.getItems().addAll("Apple", "Carrot", "Cheesecake", "Chocolate","Coffee","Opera","Tiramisu");
+        comboBox.getItems().addAll("Apple", "Carrot", "Cheesecake", "Chocolate", "Coffee", "Opera", "Tiramisu");
         comboBox.setPromptText("Select one");
-        
+
         Label lcake_size = new Label("Choose Cake Size");
-        
+
         ToggleGroup sizeGroup = new ToggleGroup();
         RadioButton smallRadio = new RadioButton("Small");
         RadioButton mediumRadio = new RadioButton("Medium");
         RadioButton largeRadio = new RadioButton("Large");
-        
+
         smallRadio.setToggleGroup(sizeGroup);
         mediumRadio.setToggleGroup(sizeGroup);
         largeRadio.setToggleGroup(sizeGroup);
-        
+
         HBox sizeBox = new HBox(10, smallRadio, mediumRadio, largeRadio);
-        
+
+        CheckBox deliveryCheck = new CheckBox("Customer lives within free delivery area");
+
+        deliveryCheck.setOnAction(e -> {
+            if (deliveryCheck.isSelected()) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Free Delivery Area");
+                alert.setHeaderText(null);
+                alert.setContentText("Customer is within the free delivery area!");
+                alert.showAndWait();
+            }
+        });
+
         Button quitBtn = new Button("Quit");
         quitBtn.setStyle("-fx-background-color: #add8e6;");
-        
-        quitBtn.setOnAction(new EventHandler<ActionEvent>(){
+
+        quitBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                 stage.close();
             }
         });
-        
+
         Button saveBtn = new Button("Save");
         saveBtn.setStyle("-fx-background-color: #add8e6;");
-        
+
         HBox hname = new HBox(15, lname, name);
         HBox hphone = new HBox(15, lphone, phone);
-        
+
         HBox btnBox = new HBox(10, quitBtn, saveBtn);
-             
-        VBox vbox = new VBox(15, hname, hphone, lcake_type, comboBox, lcake_size, sizeBox, btnBox);
+
+        VBox vbox = new VBox(15, hname, hphone, lcake_type, comboBox, lcake_size, sizeBox, deliveryCheck, btnBox);
         vbox.setPadding(new Insets(15, 12, 13, 14));
-        
-        Scene scene = new Scene(vbox, 400, 300);
-        
+
+        Scene scene = new Scene(vbox, 400, 320);
+
         stage.setScene(scene);
         stage.setTitle("Bakery: Customers' Orders");
         stage.show();
     }
-
     /**
      * @param args the command line arguments
      */
